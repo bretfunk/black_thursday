8cf72b3ce23fb737a787e6a84d101628f5cc4106
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-05-31 12:36:07 -0600

    working on item and item repo

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -5,5 +5,28 @@ require './lib/merchant'
 
 class MerchantTest < Minitest::Test
 
+  def test_that_merchant_exists
+    m = Merchant.new({:id => 5, :name => "Turing School"})
 
+    assert_instance_of m, Merchant
+  end
+
+  def test_it_returns_id_of_merchant
+    m = Merchant.new({:id => 5, :name => "Turing School"})
+
+    assert_equal 5, m.id
+  end
+
+  def test_it_returns_name_of_merchant
+    m = Merchant.new({:id => 5, :name => "Turing School"})
+
+    assert_equal "Turing School", m.name
+  end
+
+  def test_it_returns_a_time_created_at ## for item
+    time_created_at = Time.now
+    m = Merchant.new({:id => 5, :name => "Turing School", :created_at => time_created_at})
+
+    assert_equal time_created_at, m.created_at
+  end
 end
