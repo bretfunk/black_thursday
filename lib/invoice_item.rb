676ed314b08236cfaf365331ae84db87eeb80535
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-06-04 14:21:09 -0600

    transaction on iteration 3

---
+++ lib/invoice_item.rb
@@ -0,0 +1,34 @@
+require 'pry'
+require 'time'
+require 'bigdecimal'
+require 'bigdecimal/util'
+
+
+###need to find distinction between iteration 2 invoice
+class InvoiceItem
+
+  attr_reader :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at
+
+  def initialize(params, iir)
+    @iir        = iir
+    @id         = params[:id].to_i
+    @item_id    = params[:item_id]
+    @invoice_id = params[:invoice_id]
+    @quantity   = params[:quantity]
+    @unit_price = (to_big_decimal(params[:unit_price].to_f) / 100) unless params[:unit_price] == nil
+    @created_at = params[:created_at]
+    @updated_at = params[:updated_at]
+  end
+
+
+  def unit_price_to_dollars
+    @unit_price.to_f.round(2)
+  end
+
+  def to_big_decimal(price)
+    BigDecimal.new(price, price.to_s.length)
+  end
+
+  def
+
+end
---
+++ lib/transaction.rb
@@ -0,0 +1,24 @@
+require 'pry'
+require 'time'
+
+
+class Transaction
+
+  attr_reader :id, :invoice_id, :credit_card_number, :credit_card_expiration, :result, :created_at, :updated_at
+
+  def initialize(params, tr)
+    @tr = tr
+    @id = params[:id].to_i
+    @invoice_id = params[:invoice_id].to_i
+    @credit_card_number = params[:credit_card_number]
+    @credit_card_expiration = params[:credit_card_expiration]
+    @result = params[:result]
+    @created_at = params[:created_at]
+    @updated_at = params[:updated_at]
+  end
+
+  def transaction_records
+    tr.pass_to_se(purchase)##may need to change name 
+  end
+
+end
